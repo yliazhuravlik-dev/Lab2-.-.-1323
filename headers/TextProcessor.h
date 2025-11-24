15d3d3959eac347ba004556005a82fc694a87fe3
yliazhuravlik-dev <yliazhuravlik@gmail.com>
2025-11-24 15:53:14 +0300

    Update TextProcessor.h

--- headers/TextProcessor.h
+++ headers/TextProcessor.h
@@ -1,15 +1,14 @@
-#ifndef TEXTPROCESSOR_H  // Защита от повторного включения файла
-#define TEXTPROCESSOR_H  // Определение макроса для этого файла
+// TextProcessor.h
+#ifndef TEXTPROCESSOR_H
+#define TEXTPROCESSOR_H
 
-#include <fstream>       // Для работы с файловыми потоками (ifstream, ofstream)
-#include <string>        // Для использования класса string
-
-class TextProcessor {    // Объявление класса для обработки текста
+class TextProcessor {
 public:
     // Статический метод для обращения порядка предложений в файле
-    // static - метод можно вызывать без создания объекта класса
-    // const char* filename - параметр: путь к файлу для обработки
     static void reverseSentences(const char* filename);
+    
+    // Альтернативный метод без использования STL контейнеров
+    static void processTextWithoutSTL(const char* filename);
 };
 
-#endif // TEXTPROCESSOR_H // Конец защитного блока
+#endif
