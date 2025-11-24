50f7ac964b4bbcee8e561b51adbe9cc665e3e0af
yliazhuravlik-dev <yliazhuravlik@gmail.com>
2025-11-24 16:02:42 +0300

    Update AEROFLOT.h

--- headers/AEROFLOT.h
+++ headers/AEROFLOT.h
@@ -1,44 +1,48 @@
 // AEROFLOT.h
-#ifndef AEROFLOT_H  // Защита от повторного включения файла
-#define AEROFLOT_H  // Если AEROFLOT_H не определен, определяем его
+#ifndef AEROFLOT_H
+#define AEROFLOT_H
 
-#include <iostream>  // Для работы с потоками ввода/вывода (std::cout, std::cin)
-#include <cstring>   // Для работы со строками в стиле C (strcpy, strlen)
+#include <iostream>
+#include <cstring>
 
-class AEROFLOT {     // Объявление класса AEROFLOT
-private:             // Секция приватных членов класса (доступны только внутри класса)
-    char* destination;    // Указатель на строку - пункт назначения рейса
-    int flightNumber;     // Целое число - номер рейса
-    char* aircraftType;   // Указатель на строку - тип самолета
+class AEROFLOT {
+private:
+    char* destination;
+    int flightNumber;
+    char* aircraftType;
 
-public:              // Секция публичных членов класса (доступны извне)
-    // Конструкторы - специальные методы для создания объектов
-    AEROFLOT();                              // Конструктор по умолчанию (без параметров)
-    AEROFLOT(const char* dest, int number, const char* type);  // Конструктор с параметрами
-    AEROFLOT(const AEROFLOT& other);        // Конструктор копирования
+public:
+    // Конструкторы
+    AEROFLOT();
+    AEROFLOT(const char* dest, int number, const char* type);
+    AEROFLOT(const AEROFLOT& other);
     
-    // Деструктор - специальный метод для очистки памяти при уничтожении объекта
+    // Деструктор
     ~AEROFLOT();
     
-    // Оператор присваивания - для копирования данных из одного объекта в другой
+    // Оператор присваивания
     AEROFLOT& operator=(const AEROFLOT& other);
     
-    // Get-функции - методы для чтения приватных полей (геттеры)
-    const char* getDestination() const { return destination; }   // Возвращает пункт назначения
-    int getFlightNumber() const { return flightNumber; }         // Возвращает номер рейса
-    const char* getAircraftType() const { return aircraftType; } // Возвращает тип самолета
+    // Get-функции
+    const char* getDestination() const { return destination; }
+    int getFlightNumber() const { return flightNumber; }
+    const char* getAircraftType() const { return aircraftType; }
     
-    // Set-функции - методы для установки значений приватных полей (сеттеры)
-    void setDestination(const char* dest);    // Устанавливает пункт назначения
-    void setFlightNumber(int number) { flightNumber = number; }  // Устанавливает номер рейса
-    void setAircraftType(const char* type);   // Устанавливает тип самолета
+    // Set-функции
+    void setDestination(const char* dest);
+    void setFlightNumber(int number) { flightNumber = number; }
+    void setAircraftType(const char* type);
     
-    // Перегрузка операторов ввода/вывода - для удобной работы с потоками
-    friend std::ostream& operator<<(std::ostream& os, const AEROFLOT& flight);  // Вывод в поток
-    friend std::istream& operator>>(std::istream& is, AEROFLOT& flight);        // Ввод из потока
+    // Перегрузка операторов ввода/вывода
+    friend std::ostream& operator<<(std::ostream& os, const AEROFLOT& flight);
+    friend std::istream& operator>>(std::istream& is, AEROFLOT& flight);
     
     // Методы для сравнения (для сортировки)
-    bool operator<(const AEROFLOT& other) const;  // Оператор меньше для сортировки по алфавиту
+    bool operator<(const AEROFLOT& other) const;
 };
 
-#endif  // Конец защиты от повторного включения
+// Объявления операторов ВНЕ класса
+std::ostream& operator<<(std::ostream& os, const AEROFLOT& flight);
+std::istream& operator>>(std::istream& is, AEROFLOT& flight);
+
+#endif
