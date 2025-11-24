b643a75c661d013d77d19296c9f6b5a3b313b2a9
yliazhuravlik-dev <yliazhuravlik@gmail.com>
2025-11-24 16:03:13 +0300

    Update AEROFLOT.cpp

--- src/AEROFLOT.cpp
+++ src/AEROFLOT.cpp
@@ -10,39 +10,47 @@ AEROFLOT::AEROFLOT() : destination(nullptr), flightNumber(0), aircraftType(nullp
 
 // Конструктор с параметрами
 AEROFLOT::AEROFLOT(const char* dest, int number, const char* type) : flightNumber(number) {
-    destination = new char[strlen(dest) + 1];
-    strcpy(destination, dest);
+    if (dest) {
+        destination = new char[strlen(dest) + 1];
+        strcpy(destination, dest);
+    } else {
+        destination = nullptr;
+    }
     
-    aircraftType = new char[strlen(type) + 1];
-    strcpy(aircraftType, type);
+    if (type) {
+        aircraftType = new char[strlen(type) + 1];
+        strcpy(aircraftType, type);
+    } else {
+        aircraftType = nullptr;
+    }
     
-    std::cout << "Вызван конструктор с параметрами для AEROFLOT: " << destination << std::endl;
+    std::cout << "Вызван конструктор с параметрами для AEROFLOT" << std::endl;
 }
 
 // Конструктор копирования
 AEROFLOT::AEROFLOT(const AEROFLOT& other) : flightNumber(other.flightNumber) {
-    destination = new char[strlen(other.destination) + 1];
-    strcpy(destination, other.destination);
+    if (other.destination) {
+        destination = new char[strlen(other.destination) + 1];
+        strcpy(destination, other.destination);
+    } else {
+        destination = nullptr;
+    }
     
-    aircraftType = new char[strlen(other.aircraftType) + 1];
-    strcpy(aircraftType, other.aircraftType);
+    if (other.aircraftType) {
+        aircraftType = new char[strlen(other.aircraftType) + 1];
+        strcpy(aircraftType, other.aircraftType);
+    } else {
+        aircraftType = nullptr;
+    }
     
-    std::cout << "Вызван конструктор копирования для AEROFLOT: " << destination << std::endl;
+    std::cout << "Вызван конструктор копирования для AEROFLOT" << std::endl;
 }
 
 // Деструктор
 AEROFLOT::~AEROFLOT() {
-    std::cout << "Вызван деструктор для AEROFLOT: ";
-    if (destination) {
-        std::cout << destination;
-        delete[] destination;
-        destination = nullptr;
-    }
-    if (aircraftType) {
-        delete[] aircraftType;
-        aircraftType = nullptr;
-    }
-    std::cout << std::endl;
+    std::cout << "Вызван деструктор для AEROFLOT" << std::endl;
+    delete[] destination;
+    delete[] aircraftType;
 }
 
 // Оператор присваивания
@@ -53,13 +61,21 @@ AEROFLOT& AEROFLOT::operator=(const AEROFLOT& other) {
         delete[] aircraftType;
         
         // Копируем новые данные
-        destination = new char[strlen(other.destination) + 1];
-        strcpy(destination, other.destination);
+        if (other.destination) {
+            destination = new char[strlen(other.destination) + 1];
+            strcpy(destination, other.destination);
+        } else {
+            destination = nullptr;
+        }
         
         flightNumber = other.flightNumber;
         
-        aircraftType = new char[strlen(other.aircraftType) + 1];
-        strcpy(aircraftType, other.aircraftType);
+        if (other.aircraftType) {
+            aircraftType = new char[strlen(other.aircraftType) + 1];
+            strcpy(aircraftType, other.aircraftType);
+        } else {
+            aircraftType = nullptr;
+        }
     }
     return *this;
 }
@@ -67,25 +83,41 @@ AEROFLOT& AEROFLOT::operator=(const AEROFLOT& other) {
 // Set-функции
 void AEROFLOT::setDestination(const char* dest) {
     delete[] destination;
-    destination = new char[strlen(dest) + 1];
-    strcpy(destination, dest);
+    if (dest) {
+        destination = new char[strlen(dest) + 1];
+        strcpy(destination, dest);
+    } else {
+        destination = nullptr;
+    }
 }
 
 void AEROFLOT::setAircraftType(const char* type) {
     delete[] aircraftType;
-    aircraftType = new char[strlen(type) + 1];
-    strcpy(aircraftType, type);
+    if (type) {
+        aircraftType = new char[strlen(type) + 1];
+        strcpy(aircraftType, type);
+    } else {
+        aircraftType = nullptr;
+    }
 }
 
-// Перегрузка оператора вывода
+// Перегрузка оператора вывода (РЕАЛИЗАЦИЯ)
 std::ostream& operator<<(std::ostream& os, const AEROFLOT& flight) {
-    os << "Рейс №" << flight.flightNumber 
-       << " в '" << (flight.destination ? flight.destination : "null") 
-       << "' на самолете '" << (flight.aircraftType ? flight.aircraftType : "null") << "'";
+    os << "Рейс №" << flight.flightNumber;
+    if (flight.destination) {
+        os << " в '" << flight.destination << "'";
+    } else {
+        os << " в 'неизвестно'";
+    }
+    if (flight.aircraftType) {
+        os << " на самолете '" << flight.aircraftType << "'";
+    } else {
+        os << " на самолете 'неизвестно'";
+    }
     return os;
 }
 
-// Перегрузка оператора ввода
+// Перегрузка оператора ввода (РЕАЛИЗАЦИЯ)
 std::istream& operator>>(std::istream& is, AEROFLOT& flight) {
     char buffer[100];
     
@@ -105,7 +137,7 @@ std::istream& operator>>(std::istream& is, AEROFLOT& flight) {
 
 // Оператор сравнения для сортировки
 bool AEROFLOT::operator<(const AEROFLOT& other) const {
-    if (destination == nullptr || other.destination == nullptr) {
+    if (!destination || !other.destination) {
         return false;
     }
     return strcmp(destination, other.destination) < 0;
